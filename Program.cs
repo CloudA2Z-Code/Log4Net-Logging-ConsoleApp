43b40dfeb060bf7969844cb0c5c8114e5b99e822
The Azure Guy <theazureguy007@gmail.com>
2018-11-02 23:37:54 -0700

    Make Following changes step by step in the App.config,programmes.cs , Step 3 can be skipped in few cases

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Step 3
+//This also can help in configuring log4net inside the Code itself
+[assembly: log4net.Config.XmlConfigurator(Watch =true)]
+
+namespace Log4NetTest
+{
+    class Program
+    {
+        //Step 4
+        // You need to define new GetLogger names when u define in diff class or use reflection but avoid in resource intensive programmes
+        //private static readonly log4net.ILog log = log4net.LogManager.GetLogger("Program.cs");
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Helllo logging");
+            log.Error("This is my error message");
+            Console.ReadLine();
+        }
+    }
+}
